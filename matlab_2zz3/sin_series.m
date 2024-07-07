ed7a462996f84d306976e11672d679a6b1e929c1
yiakonale <yiakonale@hotmail.com>
2024-07-06 23:40:08 -0400

    lab1 final

--- matlab_2zz3/sin_series.m
+++ matlab_2zz3/sin_series.m
@@ -1,4 +1,4 @@
-%approximate x^2 0<x<L using the cosine half-range expansion
+%approximate x^2 0<x<L using the sine half-range expansion
 clear;
 
 a = zeros(1,5);
@@ -9,38 +9,38 @@ w = 2*pi/T;
 x = linspace(lower,upper,200);
 f = x.^2;
 
-a0 = (1/T)*trapz(x,f);
+%a0 = (1/T)*trapz(x,f);
 
-for n = 1:5
-    y = f.*cos(n*w*x/2);
+for n = 1:10
+    y = f.*sin(n*w*x/2);
     a(n) = (2/T)*trapz(x,y);
 end
 
 fh = zeros(1,200);
 
-for n = 1:5
-    fh = fh + a(n)*cos(n*w*x/2);
+for n = 1:10
+    fh = fh + a(n)*sin(n*w*x/2);
 end
 
-fh = a0 + fh;
+%fh = a0 + fh;
 
 plot(x,f,'r',x,fh,'b')
 legend('f','fh','Location','best')
 title('y=x^2,  0 < x < 3')
 
 x = linspace(-3,0,200);
-f = x.^2;
+f = -x.^2;
 
 fh = zeros(1,200);
 
-for n = 1:5
-    fh = fh + a(n)*cos(n*w*x/2);
+for n = 1:10
+    fh = fh + a(n)*sin(n*w*x/2);
 end
 
-fh = a0 + fh;
+%fh = a0 + fh;
 
 hold on
 plot(x,f,'r',x,fh,'b')
 legend('f','fh','Location','best')
-title('Even extension of y = x^2,  -3 < x < 3')
+title('Odd extension of y = x^2,  -3 < x < 3')
 hold off;
